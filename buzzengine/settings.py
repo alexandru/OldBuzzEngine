320a43c307ec450a384d0efc7f36c32a48e9ee34
Alexandru Nedelcu <noreply@alexn.org>
2011-03-25 17:13:23 +0200

    fix

--- buzzengine/settings.py
+++ buzzengine/settings.py
@@ -18,4 +18,4 @@ TEMPLATE_DIRS = (
 ROOT_URLCONF = 'buzzengine.urls'
 
 ADMIN_EMAIL  = "contact@alexn.org"
-EMAIL_SENDER = "TheBuzzEngine <noreply@thebuzzengine.appspot.com>"
+EMAIL_SENDER = "TheBuzzEngine <me@alexn.org>"
